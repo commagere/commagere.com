5aa28f270d42050ce65ff486f450189a7c9f7436
Blake Commagere <blake.commagere@gmail.com>
2015-08-06 22:08:49 +0000

    adding default error stubs adding static handlers

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write('Hello world 2!')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
