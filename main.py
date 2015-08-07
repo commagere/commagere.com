3fb78c693090afb5f8d8da9f72e2e4b77218dfab
Blake Commagere <blake.commagere@gmail.com>
2015-08-07 00:06:00 +0000

    Basic bootstrap template Including json-ld info

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+import os
+from datetime import date
+import jinja2
 import webapp2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world 2!')
+        
+        current_year = date.today().year
+        
+        template_values = {
+            'current_year': current_year,
+        }
 
+        template = JINJA_ENVIRONMENT.get_template('views/index.html')
+        self.response.write(template.render(template_values))
+    
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
-], debug=True)
+], debug=False)
