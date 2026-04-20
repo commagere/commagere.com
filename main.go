5d8f354ae4b77aef2a233ab46daf3701bb0550ed
Blake Commagere <blake.commagere@gmail.com>
2026-04-19 20:09:50 -0400

    Stub for template design
    Reorganizing templates to make use of templates

--- main.go
+++ main.go
@@ -25,10 +25,26 @@ func handle(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// tmpl := template.Must(template.ParseFiles("templates/index.html"))
-	tmpl := template.Must(template.ParseGlob("templates/*.html"))
-	// tmpl.Execute(w, nil)
-	err := tmpl.ExecuteTemplate(w, "base", nil)
+	layout := "base"
+	page := "index"
+
+	layoutFilename := "templates/layouts/" + layout + ".html"
+	pageFilename := "templates/pages/" + page + ".html"
+
+	partialsFilenames := []string{
+		"templates/partials/header.html",
+		"templates/partials/footer.html",
+		"templates/partials/navigation.html",
+	}
+
+	templateFilenames := append([]string{layoutFilename, pageFilename}, partialsFilenames...)
+
+	tmpl := template.Must(template.ParseFiles(templateFilenames...))
+
+	//tmpl := template.Must(template.ParseFiles(layoutFilename, pageFilename, partialsFilenames...))
+	// tmpl := template.Must(template.ParseGlob("templates/index.html"))
+	// err := tmpl.Execute(w, nil)
+	err := tmpl.ExecuteTemplate(w, layout, nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
